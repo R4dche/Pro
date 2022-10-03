b11f765b358e3447e155e7f713167a635c4f19e7
R4dche <radchenko.g97@gmail.com>
2022-10-03 21:01:44 +0300

    w1

---
+++ lesson01/index.js
@@ -0,0 +1,18 @@
+// alert("Я - JavaScript!");
+// alert("My name is Jora");
+// alert(3 * 2);
+//
+// prompt("Я - JavaScript!");
+// prompt("My name is Jora");
+// prompt(3 * 2);
+// 
+const question = prompt("What is you name?");
+const question2 = prompt("Where are u from?");
+const question3 = prompt("Do you like Ukraine?");
+
+const response = `
+    Your name is ${question};
+    Are u from ${question2};
+    Me too ${question3};
+`
+console.log(response)
